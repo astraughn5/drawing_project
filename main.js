ee9ef10ed4e4402e2e2a1861134ca26b7430297b
astraughn5 <astraughn5@gmail.com>
2022-04-28 14:04:59 -0500

    uploading animation practice with canvas

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // add an event listener to load drawing initially
 window.addEventListener('load',draw)
-document.querySelector('button').addEventListener('click',draw)
+// document.querySelector('button').addEventListener('click',draw)
 
 // set a global variable for canvas
 const canvas = document.querySelector("canvas");
@@ -15,7 +15,7 @@ const resize = () => {
   resize()
 
   // whenever the window is resized, the resize function and draw function is called
-  window.addEventListener('resize', resize)
+//   window.addEventListener('resize', resize)
 //   window.addEventListener('resize', draw)
 
 
@@ -23,61 +23,70 @@ const resize = () => {
 function draw(){
 if (canvas.getContext){
 	const ctx = canvas.getContext('2d')
-  ctx.fillStyle ='orange'
-  const size = 30
-  
-  for (let x = 0; x < canvas.width - 14; x+= size){
-  let x = 0
-  const id = setInterval(() => {
-	ctx.clearRect(0,0,canvas.width,canvas.height)
-	ctx.beginPath()
-	ctx.arc(x,60,25,0, Math.PI * 2)
-	ctx.fill()
-	x+= size
-
-	if (x>= canvas.width - 18){
-		clearInterval(id)
+  	ctx.fillStyle ='blueviolet'
+	ctx.font = '50px Arial'
+	ctx.textAlign ='center'
+	ctx.fillText('Do-a-Dot Art Marker Animation Practice',canvas.width / 2, canvas.height / 2)
+  	const size = 30
+	for (let x = 30; x < canvas.width; x+= size){
+		let x = 30
+  		const right = setInterval(() => {
+		// ctx.clearRect(0,0,canvas.width,canvas.height)
+		ctx.beginPath()
+		ctx.arc(x,60,25,0, Math.PI * 2)
+		ctx.fill()
+		x+= size
+	if (x>= canvas.width - 30){
+		clearInterval(right)
 	}
   }, 200)}
+  let y = 60
+  x = canvas.width - 30
+  	for (let y = 60; y < canvas.height; y+= size){
+		let y = 60
+		const rightDown = setInterval(() => {
+  		// ctx.clearRect(0,0,canvas.width,canvas.height)
+  		ctx.beginPath()
+  		ctx.arc(x,y,25,0, Math.PI * 2)
+  		ctx.fill()
+  		y+= size
+  			if (y>= canvas.height - 18){
+  				clearInterval(rightDown)
+  			}
+  		}
+  		,200)}
 
-}
+	let z = 60
+	for (let z = 60; z < canvas.height; z+= size){
+		let z = 60
+		const leftDown = setInterval(() => {
+		// ctx.clearRect(0,0,canvas.width,canvas.height)
+		ctx.beginPath()
+		ctx.arc(30,z,25,0, Math.PI * 2)
+		ctx.fill()
+		z+= size
+			if (z>= canvas.height - 18){
+				clearInterval(leftDown)
+			}
+		}
+		,200)}
+		
+		ctx.fillStyle ='cornflowerblue'
+		for (let x = 30; x < canvas.width; x+= size){
+			let x = 30
+			  const right = setInterval(() => {
+			// ctx.clearRect(0,0,canvas.width,canvas.height)
+			ctx.beginPath()
+			ctx.arc(x,canvas.height - 30,25,0, Math.PI * 2)
+			ctx.fill()
+			x+= size
+		if (x>= canvas.width - 30){
+			clearInterval(right)
+		}
+	  }, 200)}
+		
+	}
 else {
   console.log('Canvas-Unspported')
 }
 }
-
-// let circles = []
-// let started = false
-// function play(){
-// 	circles.push(0)
-
-// 	if (!started){
-// 		started = true
-// 		setInterval(() => {
-// 			tick()
-// 		},200)
-// 	}
-// }
-// function tick(){
-// 	ctx.clearRect(0,0,canvas.width,canvas.height)
-
-// 	circles.forEach(x => {
-// 		ctx.beginPath()
-// 		ctx.arc(x,60,25,0, Math.PI * 2)
-// 		ctx.fill()
-// 	})
-// 	circles = circles.map(x => x+= size)
-// 	.filter(x => x < canvas.width - 30)
-// 	}
-
-
-// for (let i = 0; i < 2; i++){
-// 	ctx.beginPath()
-// 	let x = 100 + i * 300
-// 	let y = 100 + i * 0
-// 	let radius = 50
-// 	let startAngle = 0
-// 	let endAngle = Math.PI * 2
-	
-// 	ctx.arc(x,y,radius,startAngle, endAngle)
-// 	ctx.fill()
